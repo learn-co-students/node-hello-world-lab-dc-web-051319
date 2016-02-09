b71dce5596a623d6a2c9cb54c4a7578e502e062f
Azat Mardan <hi@azat.co>
2016-02-09 13:39:51 -0800

    add changes :airplane:

--- test/index.js
+++ test/index.js
@@ -1,17 +1,17 @@
-var test = require('tape'),
+var expect = require('chai').expect,
   cp = require('child_process')
 
-test('node hello-world', function (t) {
-  t.plan(2)
-  child = cp.exec('node hello-world',
-  function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout)
-    console.log('stderr: ' + stderr)
-    t.equal(stderr, '')
-    if (error !== null) {
-      console.log('exec error: ' + error)
-    }
-    stdout = stdout.replace('\n','')
-    t.equal(stdout, 'Hello World')
+describe('node hello-world', function () {
+  it('must be 2.14.15 or greater', function(done){
+    child = cp.exec('node hello-world',
+    function (error, stdout, stderr) {
+      expect(stderr).to.equal('')
+      if (error !== null) {
+        console.log('exec error: ' + error)
+      }
+      stdout = stdout.replace('\n','')
+      expect(stdout).to.equal('Hello World')
+      done()
+    })
   })
 })

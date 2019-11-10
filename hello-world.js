c9f095b55edbc72b95c362abe372155e948e1392
villatrue <scarter0313@gmail.com>
2019-11-10 00:47:34 -0500

    Done.

--- hello-world.js
+++ hello-world.js
@@ -1,8 +1,8 @@
 var helloWorld = function(){
      return 'Hello World'}
 
-     if (require.main === module) { // this is true when you run this file with node hello-world.js and false when you import it
-        console.log(helloWorld()) // for node hello-world.js
-      } else {
-        module.exports = helloWorld // for require('./hello-world.js')
-      }
+if (require.main === module) { // this is true when you run this file with node hello-world.js and false when you import it
+    console.log(helloWorld()) // for node hello-world.js
+} else {
+    module.exports = helloWorld // for require('./hello-world.js')
+}
